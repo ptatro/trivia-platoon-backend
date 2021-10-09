4e2db84eb722f1882ffb2dd23198523d4483c593
Cynthia Nikolai <cynthianikolai@gmail.com>
2021-10-09 19:11:52 -0400

    trying out the channels layer again

--- trivia/settings.py
+++ trivia/settings.py
@@ -252,7 +252,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("localhost", 6379)],
+            "hosts": [("redis", 6379)],
         },
         # "ROUTING": "chat.routing.channel_routing",
     },
