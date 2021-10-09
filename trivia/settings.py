05d8c044e5539a104d79bde5ab2e9f5412c30bd9
Cynthia Nikolai <cynthianikolai@gmail.com>
2021-10-09 15:45:14 -0400

    added redis and redis cache to settings file

--- trivia/settings.py
+++ trivia/settings.py
@@ -101,7 +101,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = ['*.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['bcp-trivia.herokuapp.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
